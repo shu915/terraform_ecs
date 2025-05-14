c6c9b4ec5499b898245e1908579fc66f00d61fcf
shu915 <chukichi915@gmail.com>
2025-05-14 12:49:08 +0900

    タスクロールとタスク実行ロールを作成

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = ">= 1.7.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 5.40.0"
     }
   }
 }
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   profile = "terraform"
 }
 
